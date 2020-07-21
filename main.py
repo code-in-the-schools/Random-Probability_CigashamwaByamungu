15a4162072d193dfd164fbfb9e1845622f7d5c9c
Cigashamwa byamungu <ebyamungu28@gmail.com>
2020-07-21 15:41:33 +0000

    Everything

--- main.py
+++ main.py
@@ -4,10 +4,30 @@ import random
 for x in range(1, 6):
   print(x) 
 
+#Just trying something 
+list1 = [1, 2, 3, 4, 5, 6]
+list1.extend(list1)
+print (list1)
 
-# experiment with this
 
-list1 = [1, 2, 3, 4, 5]
- for x in range: print (list1)
+#experiment1
+List2 = ['1', '2', '3', '4', '5', '6']
+List2 = len(List2)
+for x in range(1, List2): 
+  print(x)
 
+#experiemnt2
+start = 1
+stop = 6
+step = +1
+for number in range(start, stop, step):
+  print(number, end= ', ')
 
+P1 = []
+for i in range(10)
+x = random.randrange (1, 5)
+P1.append
+
+P2
+
+   
